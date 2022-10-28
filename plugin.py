2bbcf4e405ffd8a6a7e714a1a9d468aaf89539ec
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 22:29:08 +0000

    Adding tarfile member sanitization to extractall()

--- plugin.py
+++ plugin.py
@@ -108,7 +108,26 @@ class ServerResource:
 
                         # extract and copy the cache
                         with tarfile.open(tmp_file) as tf:
-                            tf.extractall(self._cache_path)
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(tf, self._cache_path)
 
                         os.unlink(tmp_file)
 
